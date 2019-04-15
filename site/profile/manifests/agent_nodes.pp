ebfc7fda9082bd5fb497372c963b2621a5c7fd48
jinwangzheng <jinwangzheng@hotmail.com>
2019-04-15 14:56:49 +0800

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -4,4 +4,8 @@ class profile::agent_nodes {
 dockeragent::node { 'web.puppet.vm': }
   dockeragent::node { 'db.puppet.vm': }
 
+host { 'web.puppet.vm':
+ensure => present,
+ip     => '172.18.0.3',
+
 }
