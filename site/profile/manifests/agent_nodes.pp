eaa63a3d5771edf33a9d8cbfeed914ba24783fb2
jinwangzheng <jinwangzheng@hotmail.com>
2019-04-15 13:37:03 +0800

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,14 +1,6 @@
 class profile::agent_nodes {
 
-  docker::run { 'db.puppet.vm':
-    ensure => absent,
-    image   => 'agent',
-  }
-
-  docker::run { 'web.puppet.vm':
-    ensure => absent,
-    image   => 'agent',
-  }
+ 
 
 
 }
